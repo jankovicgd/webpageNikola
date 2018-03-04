cb7956070caf6e036122a9014b62fddde08f41d4
jankovicgd <jankovic.gd@gmail.com>
2018-03-04 12:40:29 +0100

    works on windows not on linux

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
- express = require('express');
-var path = require('path');
+var express =   require('express'),
+    path    =   require('path');
 
 var app = express();
 
-var assetsPath = path.join(__dirname, 'public/')
+// Setting the path to static folder
+var assetsPath = path.join(__dirname, 'public')
 console.log(assetsPath);
-app.use(express.static(assetsPath));
 
+// Specifying to use the path as static
+app.use('/public', express.static(assetsPath));
+
+// Home get route
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// Listen on this port
 app.listen(8080);
 console.log('Server running at http://localhost:8080/');
